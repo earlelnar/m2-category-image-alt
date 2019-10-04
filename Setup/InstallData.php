66de2864a0f0b937f6b6ec53f60b257a31a0d6b4
unknown <14094984+mazestricks@users.noreply.github.com>
2019-10-04 17:56:23 +0800

    Upload Extension

---
+++ Setup/InstallData.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace MageTim\CategoryImageAltTags\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallData implements InstallDataInterface
+{
+    /**
+     * EAV setup factory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Init
+     *
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        /**
+         * Add attributes to the eav/attribute
+         */
+
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Category::ENTITY,
+            'magetim_category_image_alt',
+            [
+                'type' => 'text',
+                'label' => 'Category Image Alt',
+                'input' => 'text',
+                'required' => false,
+                'sort_order' => 50,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                'group' => 'Meta Information',
+            ]
+        );
+    }
+}
---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author MazeStricks
+ * Email: acenplify@gmail.com
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MageTim_CategoryImageAltTags',
+    __DIR__
+);
